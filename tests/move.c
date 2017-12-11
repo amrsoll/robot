d12e82fa3e7e09b6121fbea06c17068dbf05449a
asoll <axel.soll@telecom-paristech.fr>
2017-12-11 21:04:35 +0100

    revisions to the test file

--- tests/move.c
+++ tests/move.c
@@ -14,7 +14,6 @@ float init_sonar;
 int init_position;
 
 void move(uint8_t sn_sonar, uint8_t motors[2], float distance)
-
 {
 	FLAGS_T state;
 	float sonar_abs;
@@ -25,53 +24,39 @@ void move(uint8_t sn_sonar, uint8_t motors[2], float distance)
 	int save_curr_position;
 	uint8_t motor_left = motors[0];
 	uint8_t motor_right = motors[1];
-
 	get_tacho_position(motor_right, &position);
 	distance = (int) distance * 360/200; //TRy to change it
 
 	int final_position = position + distance;
 	multi_set_tacho_command_inx(motors,TACHO_RUN_FOREVER);
 	while(position<final_position){
-
 		if (ev3_search_sensor(LEGO_EV3_GYRO, &sn_sonar,0)){
-
 			if ( !get_sensor_value0(sn_sonar, &value )) {
 				value = init_sonar;
 			}
-
 			sonar_val = value-init_sonar;
 			sonar_abs = abs(sonar_val);
-
 			//  printf( "\r(%f) \n", sonar_val);
 			//  fflush( stdout );
-
 		}
 		//Stop if an obstacle is too close
 		if (sonar_abs > 2.0) {
 			get_tacho_position(motor_right, &save_curr_position);
 			while (sonar_abs > 2.0) {
-
 				//motors stop on both side
 				set_tacho_command_inx( motor_left, TACHO_STOP );
 				set_tacho_command_inx( motor_right, TACHO_STOP );
 				if (ev3_search_sensor(LEGO_EV3_GYRO, &sn_sonar,0)){
-
 					if ( !get_sensor_value0(sn_sonar, &value )) {
-
 						value = init_sonar;
-
 					}
-
 					sonar_val = value-init_sonar;
 					sonar_abs = abs(sonar_val);
-
 					//printf( "\r(%f) \n", sonar_val);
 					//fflush( stdout );
-
 				}
 				get_tacho_max_speed(motor_right,&maximum_robot_speed);
 				printf("Robot max speed is = %d\n",&maximum_robot_speed);
-
 				set_tacho_speed_sp( motor_right, maximum_robot_speed / 10 );
 				set_tacho_speed_sp( motor_left, maximum_robot_speed / 10 );
 				set_tacho_position_sp( motor_right, sonar_val-2 );
@@ -89,13 +74,8 @@ void move(uint8_t sn_sonar, uint8_t motors[2], float distance)
 			set_tacho_speed_sp( motor_left, maximum_robot_speed / 3);
 			multi_set_tacho_command_inx(motors,TACHO_RUN_FOREVER);
 		}
-
-
 		get_tacho_position(motor_right, &position);
-
 	}
-
 	set_tacho_command_inx( motor_left, TACHO_STOP );
 	set_tacho_command_inx( motor_right, TACHO_STOP );
-
 }

9a2035583c6dd35bad47c9824dc5ebedd3e1c3e3
vemunddahle <vemundd@stud.ntnu.no>
2017-12-11 15:27:26 +0100

    add function signatures

--- client/sensors.h
+++ client/sensors.h
@@ -1,1 +1,9 @@
-/* todo */
+#ifndef SENSORS_H
+#define SENSORS_H
+
+void sensor_init(uint8_t);
+float get_compass_value(uint8_t);
+float get_sonar_value(uint8_t);
+int get_color_value(uint8_t);
+
+#endif

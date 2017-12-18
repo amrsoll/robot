72cd594e1b568647048bf5b2bc7af8102ddf7342
vemunddahle <vemundd@stud.ntnu.no>
2017-12-18 15:26:41 +0100

    add constants

--- client/obstacle.c
+++ client/obstacle.c
@@ -1,27 +1,26 @@
 #include "obstacle.h"
 
+/* this function assumes the obstacle is hit at 90 degrees... */
 int eval_obstacle() {
     bool left, right;
 
     float distance_front, distance_left, distance_right;
 
-    get_sensor_value0(sn_gyr, &distance_front);  //should be the threshold value, 300
+    get_sensor_value0(sn_sonar, &distance_front);  //should be the threshold value, 300
 
     //check left
-    turn_absolute(motor_right, max_speed, 1, 30.0);
+    turn_absolute(mov_motors[1], max_speed, 1, TURN_DEGREE);
     Sleep(500);
-    get_sensor_value0(sn_gyr, &distance_left);
-    //do some trigonometry here
-    if(distance_left > trig_result) {
+    get_sensor_value0(sn_sonar, &distance_left);
+    if(distance_left > CHECK_DISTANCE) {
         left = false;
     } else { left = true;}
 
     //check right
-    turn_absolute(motor_left, max_speed, 1, 60.0);
+    turn_absolute(mov_motors[0], max_speed, 1, 2*TURN_DEGREE);
     Sleep(1000);
-    get_sensor_value0(sn_gyr, &distance_right);
-    //trigonometry here as well
-    if(distance_right > trig_result) {
+    get_sensor_value0(sn_sonar, &distance_right);
+    if(distance_right > CHECK_DISTANCE) {
         right = false;
     } else { right = true;}
 
@@ -35,7 +34,7 @@ int eval_obstacle() {
 
 
     //turn back to initial direction
-    turn_absolute(motor_right, max_speed, 1, 30.0);
+    turn_absolute(motor_right, max_speed, 1, TURN_DEGREE);
     Sleep(500);
 
 
---
+++ client/obstacle.h
@@ -0,0 +1,14 @@
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
+
+#include <stdbool.h>
+
+#include "gsyst.h"
+
+/* assumes sonar threshold of 15 cm */
+/* and hitting obstacles at 90 degrees... */
+#define TURN_DEGREE 33.7
+#define CHECK_DISTANCE 18.03
+
+
+#endif

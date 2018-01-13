214f98217d95610da892f0cba9ddce1e85142592
asoll <axel.soll@telecom-paristech.fr>
2018-01-13 01:17:21 +0100

    started to fill inand re-design algorithms.html. Change other pages according to that style?

--- docs/js/monjs.js
+++ docs/js/monjs.js
@@ -1,9 +1,28 @@
-$('div .expand').click(function () {                  
+/**
+ * @Author: Axel_Soll <amrsoll>
+ * @Date:   12/01/2018
+ * @Email:  axel.soll@telecom-paristech.fr
+ * @Last modified by:   amrsoll
+ * @Last modified time: 12/01/2018
+ */
+
+
+
+$('div .expand').click(function () {
     var checkedState =   $(this).prop("checked")
     $(this)
           .parent('div')
           .children('.expand:checked')
           .prop("checked", false);
-    
+
     $(this).prop("checked", checkedState);
 });
+
+function show_scan_fail(){
+    var img_scan_fail = document.getElementById('scan_fail');
+	if(button.parentElement.classList.contains('hidden')){
+		button.parentElement.classList.remove('hidden');
+	} else {
+	button.parentElement.classList.add('hidden');
+	}
+}
